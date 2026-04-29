7f1477c72ff2b3a805940927aee624b1872974dc
ludihan <65617704+ludihan@users.noreply.github.com>
2026-04-28 20:50:53 -0400

    change locale to C.UTF-8

--- nixos/default.nix
+++ nixos/default.nix
@@ -92,19 +92,8 @@
   # time.timeZone = "America/Fortaleza";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "C.UTF-8";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "C.UTF-8";
-    LC_IDENTIFICATION = "C.UTF-8";
-    LC_MEASUREMENT = "C.UTF-8";
-    LC_MONETARY = "C.UTF-8";
-    LC_NAME = "C.UTF-8";
-    LC_NUMERIC = "C.UTF-8";
-    LC_PAPER = "C.UTF-8";
-    LC_TELEPHONE = "C.UTF-8";
-    LC_TIME = "C.UTF-8";
-  };
   i18n.inputMethod = {
     type = "fcitx5";
     enable = false;
